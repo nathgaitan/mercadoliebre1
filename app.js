3614fe06d30598c97871a21f8d654f69ad1d2c1d
nathgaitan <nathalia2912dante@gmail.com>
2021-06-29 18:39:59 -0300

    mercadoliebre

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const port = 3030;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/about',(req,res)=> res.sendFile(path.join(__dirname,'views','about.html')));
+app.get('/music',(req,res) => res.sendFile(path.join(__dirname,'views','music.html')));
+app.get('/contact',(req,res)=> res.sendFile(path.join(__dirname,'views','contact.html')))
+
+app.listen(port,() => console.log(`Server running in http://localhost:${port}`) )

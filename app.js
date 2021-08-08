a967b9b34cc270aa72f9ece58fcacd70bda50054
nathgaitan <nathalia2912dante@gmail.com>
2021-08-07 22:54:10 -0300

    subiendo formularios sin terminar

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
-app.get('/about',(req,res)=> res.sendFile(path.join(__dirname,'views','about.html')));
-app.get('/music',(req,res) => res.sendFile(path.join(__dirname,'views','music.html')));
-app.get('/contact',(req,res)=> res.sendFile(path.join(__dirname,'views','contact.html')))
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
+
 
 app.listen(port,() => console.log(`Server running in http://localhost:${port}`) )
